1b4ee503a2b4bf8276b27d650d85d9416788b73b
nablec <39747645+nablec@users.noreply.github.com>
2019-02-13 17:26:09 -0800

    Add files via upload

---
+++ DB SCRIPT/allyears(2k).sql
@@ -0,0 +1,8 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf200
+{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 CREATE TABLE IF NOT EXISTS `cs540_chengxi`.`allyears2k` (`Year` int(4), `Month` int(2), `DayofMonth` int(2), `DayOfWeek` int(1), `DepTime` varchar(4), `CRSDepTime` int(4), `ArrTime` varchar(4), `CRSArrTime` int(4), `UniqueCarrier` varchar(2), `FlightNum` int(4), `TailNum` varchar(8), `ActualElapsedTime` varchar(3), `CRSElapsedTime` varchar(3), `AirTime` varchar(3), `ArrDelay` varchar(3), `DepDelay` varchar(3), `Origin` varchar(3), `Dest` varchar(4), `Distance` varchar(4), `TaxiIn` varchar(3), `TaxiOut` varchar(3), `Cancelled` varchar(2), `CancellationCode` varchar(2), `Diverted` varchar(2), `CarrierDelay` varchar(3), `WeatherDelay` varchar(3), `NASDelay` varchar(3), `SecurityDelay` varchar(2), `LateAircraftDelay` varchar(3), `IsArrDelayed` varchar(3), `IsDepDelayed` varchar(3)) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;}
